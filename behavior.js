1ba7988773d358c86feff7269edb2576c28a09f4
Alejandro <chapale2000@gmail.com>
2024-09-17 19:35:29 -0300

    js: function removeSquares added

--- behavior.js
+++ behavior.js
@@ -1,9 +1,18 @@
 const grid = document.querySelector("#grid");
 const button = document.querySelector("button");
+let numberLastGrid = 0;
+
+function deleteSquares() {
+    for(let i = 0; i < numberLastGrid; i++) {
+        let square = document.querySelector(".square");
+        grid.removeChild(square);
+    }
+}
 
 function createSquares(squaresPerSide) {
     let gridWidth = squaresPerSide * 16;
     let numberOfSquares = squaresPerSide * squaresPerSide;
+    numberLastGrid = numberOfSquares;
     grid.style.width = gridWidth + "px"; 
     
     for (let i = 0; i < numberOfSquares; i++) {
@@ -16,8 +25,10 @@ function createSquares(squaresPerSide) {
 button.addEventListener("click", () => {
     let squaresPerSide = parseInt(prompt("how many squares per side do you want? (enter a value beetween 1-100)"));
     if (squaresPerSide > 0 && squaresPerSide <= 100) {
+        if (numberLastGrid > 0) deleteSquares();
         createSquares(squaresPerSide);
     }
+
     else alert("please enter a correct value (beetween 1-100)");
 });
 
